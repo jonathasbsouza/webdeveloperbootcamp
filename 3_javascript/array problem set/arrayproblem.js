0b97455b93b5fa746f7bd9ca373364202e95b0cc
Jônathas Souza <jonathasbsouza@gmail.com>
2018-06-16 07:31:14 -0300

    array problem set
    functions printReverse(), isUniform(), sumArray() and max()

---
+++ 3_javascript/array problem set/arrayproblem.js
@@ -0,0 +1,36 @@
+let numbers = ["1", "2", "3"];
+let equal = [1,1,1,1,1,1,1,1,1];
+let numbers2 = [1,34,3456,2,33,378,9800,13,25];
+
+function printReverse(array){
+    for(let i = array.length; i>=0; i--){
+        console.log(array[i]);
+    }
+}
+
+function isUniform(array){
+    for(let i = 1; i < array.length; i++) {
+        if(array[i] !== array[0]){
+            return false;
+        }
+    }
+    return true;
+}
+
+function sumArray(array){
+    let soma = 0;
+    array.forEach(function(item){
+        soma += (+Number(item));
+    });
+    console.log(soma);
+}
+
+function max(array){
+    let maxNumber = array[0];
+    for(let i = 1; i < array.length; i++) {
+        if(array[i] > maxNumber) {
+            maxNumber = array[i];
+        }
+    }
+    console.log(maxNumber)
+}
